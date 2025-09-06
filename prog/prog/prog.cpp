aaa75f4992c1c5e1965c02162f2c1db89b5a651e
TL-hash <terem158@mail.ru>
2025-09-06 13:37:56 +0300

    Commit on PC (.txt file)

--- prog/prog/prog.cpp
+++ prog/prog/prog.cpp
@@ -8,4 +8,34 @@ int main()
 
     cout << "Enter 1st number: ";
     cin >> num1;
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter 2nd number: ";
+    cin >> num2;
+
+    switch (operation) {
+    case '+':
+        cout << "Result: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Result: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Result: " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << "Result: " << num1 / num2 << endl;
+        }
+        else {
+            cout << "Error: devision by zero!" << endl;
+        }
+        break;
+    default:
+        cout << "Wrong operation!" << endl;
+    }
+
+    return 0;
 }
